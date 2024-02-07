45096d0001c5ccd1a2db57ee0cd34243879ee6a1
CielWaaah <1622284619@qq.com>
2024-02-07 19:32:58 +0800

    fix:学号的类型统一为string

--- webcrawler/api/dormitory.go
+++ webcrawler/api/dormitory.go
@@ -15,13 +15,13 @@ func GetDormInfo(c *gin.Context) {
 	}
 }
 
-func Money(c *gin.Context) {
-	var dorm service.DomitoryService
-	var user service.UserService
-
-	err := c.ShouldBind(&user)
-
-	if err == nil {
-		//dorm.GetMoney()
-	}
-}
+//func Money(c *gin.Context) {
+//	var dorm service.DomitoryService
+//	var user service.UserService
+//
+//	err := c.ShouldBind(&user)
+//
+//	if err == nil {
+//		//dorm.GetMoney()
+//	}
+//}
--- webcrawler/model/user.go
+++ webcrawler/model/user.go
@@ -7,13 +7,13 @@ import (
 
 type User struct {
 	gorm.Model
-	Number    int       `gorm:"unique" json:"number" form:"number"` //学号
+	Number    string    `gorm:"unique" json:"number" form:"number"` //学号
 	Admission time.Time `json:"admission" form:"admission"`         //入学时间
 	Dorm      Dormitory `json:"dorm" form:"dorm"`                   //宿舍
 }
 
 // 根据学号查用户
-func (user *User) GetUserByNumber(db *gorm.DB, studentID int) (User, error) {
+func (user *User) GetUserByNumber(db *gorm.DB, studentID string) (User, error) {
 	// 查询
 	result := db.Where("number = ?", studentID).First(&user)
 	if result.Error != nil {
